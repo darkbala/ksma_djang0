5fd9d6a130a7f89e64101df33686a4b7b9bdb16e
rustam <rustam.samsaev@gmail.com>
2022-12-21 15:40:52 +0600

    rus1.6

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\IT\python\COURSE27\.venv
+set VIRTUAL_ENV=D:\IT\python\ksma_django\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
